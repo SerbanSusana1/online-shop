aca82ff06ee1d6bcfbffd4df0f0e55ca81b3dc8a
susana.serban <susana.serban@casomes.ro>
2019-10-09 19:49:52 +0300

    added cart controller and handled CrossOrigin

---
+++ src/main/java/org/fasttrackit/onlineshop/web/CartController.java
@@ -0,0 +1,40 @@
+package org.fasttrackit.onlineshop.web;
+
+import org.fasttrackit.onlineshop.service.CartService;
+import org.fasttrackit.onlineshop.transfer.cart.AddProducttoCartRequest;
+import org.fasttrackit.onlineshop.transfer.cart.CartResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/carts")
+public class CartController {
+
+    private  final CartService cartService;
+
+    @Autowired
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @PutMapping
+    public ResponseEntity addProductToCart(@RequestBody @Valid AddProducttoCartRequest request){
+        cartService.addProductToCart(request);
+        return  new ResponseEntity(HttpStatus.NO_CONTENT);
+
+    }
+
+    @GetMapping("/{id}")
+    public  ResponseEntity<CartResponse> getCart(@PathVariable("id") long customerId){
+        CartResponse card = cartService.getCard(customerId);
+
+        return new ResponseEntity<>(card,HttpStatus.OK);
+
+    }
+
+}
